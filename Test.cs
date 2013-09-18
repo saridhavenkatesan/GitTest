7996594990db76afa7399831921653d15abbde88
saridhavenkatesan <saridha.venkatesan@emids.com>
2013-09-18 09:45:44 +0530

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Medseek.Integration.PhysicianSvc
+{
+    /// <summary>
+    /// Implementation of Physician service using <see cref="IMessageOperaton"/> interface for two-way calls.
+    /// </summary>
+    public class PhysicianSvcTwoWayImplementation : IMessageOperation
+    {
+}
+}
